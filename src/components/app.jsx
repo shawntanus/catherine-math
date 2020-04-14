d686ab70fa824969316cec4d045bbd45863b654e
Shawn Tan <shawntanus@gmail.com>
2020-04-14 12:50:52 -0700

    refactored with reducer

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import NavBar from './navbar';
 import Intro from './intro';
 import Problem from './problem';
 import Summary from './summary';
+
 
 const App = () => {
   const [levels] = useState([
@@ -11,46 +12,57 @@ const App = () => {
     {id: 3, title: "Level 3", desc: "Minus under 100 with borrow", total: 10},
     {id: 4, title: "Level 4", desc: "Minus under 100 with borrow mixed", total: 10},
     ]);
-    const [shared, setShared] = useState({
-      level: levels[0],
-      right: 0,
-      wrong: 0,
-      end: null,
-      begin: null,
-      wrong_questions: []
-    });
+
+  const initShared = {
+    level: levels[0],
+    right: 0,
+    wrong: 0,
+    end: null,
+    begin: null,
+    wrong_questions: []
+  };
+
+  const [shared, dispathShared] = useReducer((state, action)=>{
+    switch (action.type) {
+      case 'setLevel':
+        return {...initShared, level:action.level}
+      case 'begin':
+        return {...state, begin: Date.now()};
+      case 'end':
+        return {...state, end: Date.now()};
+      case 'answer':
+        if(action.isRight)
+          return {...state, right: state.right+1};
+        else
+          return {...state, wrong: state.wrong+1, wrong_questions: state.wrong_questions.concat(action.wrong)};
+      default:
+        throw new Error(action.type + "has not found");
+    }
+  }, initShared);
     
-    useEffect(()=>{
-      if(shared.right+shared.wrong>=shared.level.total){
-        setShared({...shared, end: Date.now()});
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[shared.right, shared.wrong]);
-
-    const onAnswer = (isRight, wrong) => {
-      if(isRight){
-        setShared({...shared, right: shared.right+1});
-      }else{
-        setShared({...shared, wrong: shared.wrong+1, wrong_questions: shared.wrong_questions.concat(wrong) });
-      }
+  useEffect(()=>{
+    if(shared.right+shared.wrong>=shared.level.total){
+      dispathShared({type: 'end'});
     }
+  },[shared.right, shared.wrong, shared.level.total]);
 
-    const onSetLevel = (i) => {
-      setShared({...shared, level: i, right:0, wrong:0, begin: null, end: null, wrong_questions: []});
-    }
-  
-    return (
-      <div className="App">
-        <NavBar shared={shared} levels={levels} onSetLevel={onSetLevel}/>
-        <div className="container">
-          {(shared.begin==null) && 
-          <Intro shared={shared} levels={levels} onStart={()=>setShared({...shared, begin: Date.now() })}/>}
+  const onSetLevel = (i) => {
+    dispathShared({type: 'setLevel', level: i});
+  }
 
-           {(shared.begin && shared.end == null) && <Problem shared={shared} onAnswer={onAnswer} /> }
+  return (
+    <div className="App">
+      <NavBar shared={shared} levels={levels} onSetLevel={onSetLevel}/>
+      <div className="container">
+        {(shared.begin==null) && 
+          <Intro shared={shared} levels={levels} onStart={()=>dispathShared({type: 'begin'})}/>
+        }
 
-           {(shared.end) && <Summary shared={shared} levels={levels} />}
-        </div>
+        {(shared.begin && shared.end == null) && <Problem shared={shared} onAnswer={(isRight, wrong)=> dispathShared({type:'answer', isRight: isRight, wrong: wrong})} /> }
+
+        {(shared.end) && <Summary shared={shared} levels={levels} />}
       </div>
+    </div>
   );
 }
 
