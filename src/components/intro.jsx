baae916d89bbfdda56fc651e0b3a4eb593f6dcdc
Shawn Tan <shawntanus@gmail.com>
2021-07-24 15:07:31 -0700

    Wording

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Intro = (props) => {
   return (
     <div className="jumbotron">
-      <h1>Minus Level {props.shared.level.id}</h1>
+      <h1>Practice Level {props.shared.level.id}</h1>
       <p>{props.shared.level.desc}</p>
       <p>
         <button className="btn btn-lg btn-primary" onClick={props.onStart}>Start &raquo;</button>
